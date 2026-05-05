5f5d337d19c593e99b9482d7496613070fc7d5cc
Octavian-123 <uoctaviu@gmail.com>
2026-05-05 10:51:26 +0100

    Update matplotlib and pandas question 1.py

--- matplotlib and pandas question 1.py
+++ matplotlib and pandas question 1.py
@@ -3,6 +3,6 @@ import matplotlib.pyplot as plt
 
 df = pd.read_csv("marineLife.csv")
 
-filtered = df[df["habitat"].str.contains ("ocean", case = False)]
+filtered = df[df["habitat"].str.contains(ocean", case = False)]
 
 print(filtered[["species", "common_name", "habitat"]])
